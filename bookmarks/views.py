de120593301421acef515bd433ef3d018817cf2a
saai1330 <39475195+saai1330@users.noreply.github.com>
2018-05-24 09:06:26 +0530

    return html statementts

--- bookmarks/views.py
+++ bookmarks/views.py
@@ -3,4 +3,16 @@ from django.shortcuts import render
 
 # Create your views here.
 def main_page(request):
-    return HttpResponse('welcome to Django bookmarks')
+    output = '''
+    <html>
+    <head><title>%s</title></head>
+    <body>
+    <h1>%s</h1>
+    <p>%s</p>
+    </body>
+    </html>''' % (
+        'Django Bookmarks',
+        'Welcome to Django Bookmarks',
+        'Where you can store add share bookmarks!')
+
+    return HttpResponse(output)
